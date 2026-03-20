a37938268d1a9f3905643d74cc53f30944da2313
vansh Sharma <vanshsharma7832@gmail.com>
2026-03-20 23:03:51 +0530

    added logs var

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -9,3 +9,5 @@ DEVICE="cuda" if torch.cuda.is_available() else "cpu"
 SAVED_MODELS_FOLDER_PATH="models"
 EMOJI_GENERATOR_MODEL_NAME="EmojiFaceGenerator.pth"
 BG_REMOVER_MODEL_NAME="BgRemover.pth"
+
+LOGS_DIR="logs"
